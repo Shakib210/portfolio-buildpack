2121f7849b150ad9c295b6a054af3ec1a584ffde
Shakib210 <34151336+Shakib210@users.noreply.github.com>
2020-11-20 10:56:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import HomePage from './Homepage'
 import Form from '../src/Form'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-      <Router>
-
-      <Route path='/163158403' component={Form}/>
-      <Route path='/' component={HomePage} exact/>
-
-      </Router>
-        
-        
-    </div>
-  )
+class App extends React.Component {
+  componentDidMount(){
+    document.title = "Nazmus shakib"
+  }
+  render(){
+    return (
+      <div>
+        <Router>
+        <Route path='/163158403' component={Form}/>
+        <Route path='/' component={HomePage} exact/>
+        </Router>        
+      </div>
+    )
+  }
+  
 }
 
 export default App
